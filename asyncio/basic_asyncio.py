cb6c3df3e5714547cadb005f8186b81e445e0f84
Alex Y <github.alex.ai@gmail.com>
2026-03-04 11:08:42 +0100

    Add to_thread to unlock blocking function

--- asyncio/basic_asyncio.py
+++ asyncio/basic_asyncio.py
@@ -1,18 +1,36 @@
 import asyncio
+import time
+
 
 # A fast co-routine
 async def add_fast(x, y):
     print("starting fast add")
-    await asyncio.sleep(3) # Mimic some network delay
+    await asyncio.sleep(3)          # cooperative non-blocking pause
     print("result ready for FAST add")
     return x + y
 
 # A slow co-routine
 async def add_slow(x, y):
     print("starting slow add")
-    await asyncio.sleep(5) # # Mimic some network delay
+    await asyncio.sleep(5)          # cooperative non-blocking pause
     print("result ready for SLOW add")
     return x + y
+
+
+# Legacy blocking function (BAD)
+def old_blocking_bad_function():
+    print("starting OLD blocking function (will block loop if called directly)")
+    time.sleep(4)               # blocking pause
+    print("OLD blocking function finished")
+    return "blocking result"
+
+# run blocking code in thread
+async def blocking_good():
+    print("\n--- GOOD: using to_thread ---")
+    result = await asyncio.to_thread(old_blocking_bad_function)
+    print("blocking returned:", result)
+
+
 
 # Create a function to schedule co-routines
 async def get_results(mode: str):
@@ -20,7 +38,9 @@ async def get_results(mode: str):
         # background/concurrent work
         task1 = asyncio.create_task(add_slow(3, 4))
         task2 = asyncio.create_task(add_fast(5, 5))
-        print(await task1, await task2)     # Prints only when both are finished
+        task3 = asyncio.create_task(blocking_good())
+
+        print(await task1, await task2, await task3)     # Prints only when both are finished
     else:
         # print only when both are finished
         results = await asyncio.gather(
